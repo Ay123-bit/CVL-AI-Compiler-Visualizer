dd504c85d2d7f612095c4f4ec621713ee1b999c8
Ay123-bit <ayushkumar270406@gmail.com>
2026-08-02 12:22:36 +0530

    Add Navbar component with navigation links

---
+++ components->Navbar.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+
+  return (
+    <nav className="navbar">
+
+      <div className="logo">
+        CVL
+      </div>
+
+      <div className="nav-links">
+
+        <NavLink to="/">
+          Home
+        </NavLink>
+
+        <NavLink to="/dashboard">
+          Dashboard
+        </NavLink>
+
+        <NavLink to="/about">
+          About
+        </NavLink>
+
+        <NavLink to="/contact">
+          Contact
+        </NavLink>
+
+      </div>
+
+    </nav>
+  );
+}
+
+export default Navbar;
